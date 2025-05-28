c04410a274f55a859d44b21d9418d8c5c63c1fee
lamictra <lamictra@yahoo.com>
2025-05-28 15:23:01 +0800

    Update guests.js

--- guests.js
+++ guests.js
@@ -8,7 +8,7 @@ const guestList = [
   { name: "Nicholas", seat: "Table 1" },
   { name: "Christy", seat: "Table 1" },
   { name: "Dewi", seat: "Table 1" },
-  { name: "Maureen", seat: "Table 1" }
+  { name: "Maureen", seat: "Table 1" },
 
   { name: "Selena David", seat: "Table 2" },
   { name: "Dolly", seat: "Table 2" },
@@ -19,18 +19,18 @@ const guestList = [
   { name: "Kaaydan", seat: "Table 2" },
   { name: "Premasire", seat: "Table 2" },
   { name: "Sabrina", seat: "Table 2" },
-  { name: "Lorraine", seat: "Table 2" }
+  { name: "Lorraine", seat: "Table 2" },
 
-  { name: "Parovsh", seat: "Table 2" },
-  { name: "Kemmishra", seat: "Table 2" },
-  { name: "Nick", seat: "Table 2" },
-  { name: "Kuni Aziza", seat: "Table 2" },
-  { name: "Antonello", seat: "Table 2" },
-  { name: "Sheena, Jonny", seat: "Table 2" },
-  { name: "Johnny", seat: "Table 2" },
-  { name: "Hwa Pui San (Panisha)", seat: "Table 2" },
-  { name: "Hwa Pui Ling (Misha)", seat: "Table 2" },
-  { name: "Lorraine", seat: "Table 2" }
+  { name: "Parovsh", seat: "Table 3" },
+  { name: "Kemmishra", seat: "Table 3" },
+  { name: "Nick", seat: "Table 3" },
+  { name: "Kuni Aziza", seat: "Table 3" },
+  { name: "Antonello", seat: "Table 3" },
+  { name: "Sheena, Jonny", seat: "Table 3" },
+  { name: "Johnny", seat: "Table 3" },
+  { name: "Hwa Pui San (Panisha)", seat: "Table 3" },
+  { name: "Hwa Pui Ling (Misha)", seat: "Table 3" },
+  { name: "Lorraine", seat: "Table 3" }
 
 
 
